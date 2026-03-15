51511e87f53dfa39717bfe4ec86e27e3dd099ccd
Korkin-Igor <korkinigorttit@gmail.com>
2026-03-15 16:43:17 +0700

    feat(php): set up send-email php script

--- src/send-mail.php
+++ src/send-mail.php
@@ -1,1 +1,40 @@
 <?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    exit;
+}
+
+// Получаем данные
+$address = trim($_POST['address']);
+$phone = trim($_POST['phone']);
+$email = trim($_POST['email']);
+
+// Валидация
+if (!$address || !$phone) {
+    http_response_code(400);
+    exit;
+}
+
+// Формируем письмо
+$to = 'contact@obuchenie-lider.ru'; // отправляем на доменную почту
+$from = $to;
+$subject = 'Новая заявка на проверку объекта';
+
+$message = "Телефон: $phone\n";
+$message .= $email ? "Email: $email\n" : '';
+$message .= 'Дата: ' . date('d.m.Y H:i:s');
+
+$headers = "From: contact@obuchenie-lider.ru\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+// Отправляем
+if (!mail($to, $subject, $message, $headers, "-f$from")) {
+    http_response_code(500);
+}
+
+header('Location: /?success=1');
+exit;
